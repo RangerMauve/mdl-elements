4f7780b0f9dff6dfd2f6c84c9277424a79eb0f20
rangermauve <RangerMauve@hotmail.com>
2016-03-16 22:01:26 -0400

    Initial version with buttons

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mdl-elements
 A set of custom elements wrapping over Material Design Lite
+
+## Issues:
+- Currently cannot downgrade elements once they've been upgraded
---
+++ elements/button.js
@@ -0,0 +1,45 @@
+"use strict";
+var ATTRIBUTE_CLASSES = {
+	raised: "mdl-button--raised",
+	ripple: "mdl-js-ripple-effect",
+	colored: "mdl-button--colored",
+	accented: "mdl-button--accent",
+	fab: "mdl-button--fab",
+	mini: "mdl-button--mini-fab",
+	icon: "mdl-button--icon",
+};
+
+module.exports = document.registerElement("mdl-button", {
+	prototype: Object.create(
+		HTMLButtonElement.prototype, {
+			attachedCallback: {
+				value: handle_attached
+			},
+			attributeChangedCallback: {
+				value: handle_change
+			}
+		})
+});
+
+function handle_attached() {
+	this.classList.add("mdl-button");
+	this.classList.add("mdl-js-button");
+
+	for (var name in ATTRIBUTE_CLASSES) {
+		if (this.hasAttribute(name))
+			this.classList.add(ATTRIBUTE_CLASSES[name]);
+	}
+
+	window.componentHandler.upgradeElement(this);
+}
+
+function handle_change(name) {
+	var attributeClass = ATTRIBUTE_CLASSES[name];
+	if (!attributeClass) return;
+
+	var has = this.hasAttribute(name);
+
+	if (has)
+		this.classList.add(attributeClass);
+	else this.classList.remove(attributeClass);
+}
